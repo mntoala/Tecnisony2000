4c74ea46dac21ddfd8cab954bb5c6aac7e524b80
mntoala <66579043+mntoala@users.noreply.github.com>
2023-09-03 23:54:42 -0500

    Add files via upload
    Actualizacion flechas

---
+++ 11.4 Tecnisony's Page/index.js
@@ -0,0 +1,13 @@
+$(".slideToogle").slideUp();
+$(".flecha").click(function(){
+    var container = $(this).closest(".card"); // busco al contenedor padre de donde esta flecha
+    var block = container.find(".slideToogle"); // en ese contenedor busco el slideToogle
+    block.slideToggle();
+
+    // console.log($(".slideToogle"));
+});
+
+// for (var i=0; i<ncardB; i++){
+//     $(".flecha").click(function(){
+//         $(".slideToogle"+[i]).slideToggle();
+// })}
